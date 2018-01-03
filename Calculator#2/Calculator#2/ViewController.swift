cd6e01a5e8bc7b1d23f7bd96c59692838beb264a
xiaodong <345227009@qq.com>
2018-01-03 17:12:39 +0800

    完成基础平方根

--- Calculator#2/Calculator#2/ViewController.swift
+++ Calculator#2/Calculator#2/ViewController.swift
@@ -10,10 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var urerIsIntheMiddleofTyping = false //定义一个布尔值
+    
     @IBAction func touchDight(_ sender: UIButton) {
-        var digit 
+        var digit = sender.currentTitle! //接收按钮的数字
+        if urerIsIntheMiddleofTyping {
+            let textCurrentlyInDisplay = display.text! //把内容显示在display上
+            display.text = textCurrentlyInDisplay + digit //显示的内容等于显示的加上按键的
+        } else {
+            display.text = digit
+            urerIsIntheMiddleofTyping = true
+        }
     }
     
-
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        
+        }
+        set {
+        display.text = String(newValue)
+        }
+    }
+    @IBAction func performOperation(_ sender: UIButton) {
+        urerIsIntheMiddleofTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√": //定义平凡根
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
 }
-
